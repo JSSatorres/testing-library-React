560e358827ac38e2291d7a06cb352a9ad284f437
jssatorres <juansataz.dev@gmail.com>
2023-05-19 16:25:21 +0200

    feat: userForm component and style

---
+++ src/componets/UserForm/UserForm.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './user-form.css'
+
+const UserForm = () => {
+  const [formData, setFormData ] = useState({name:"",email:""});
+  const [showDiv, setShowDiv ] = useState(false);
+
+
+  function handleInputChange(e) {
+    e.preventDefault()
+    setFormData({...formData, [e.target.name]:e.target.value})
+  }
+
+  function handleSubmit(event) {
+    alert('Nombre: ' + formData.name + '\nCorreo electrónico: ' + formData.email);
+    event.preventDefault();
+  }
+  return (
+    <>
+      <button onClick={()=> setShowDiv(!showDiv)}>ver div</button>
+      {showDiv && <div> holi</div>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="nameLabel">
+            Nombre:
+          </label>
+          <input type="text" name="name" value={formData.name} onChange={handleInputChange} />
+        </div>
+        <div>
+          <label htmlFor="emailLabel" >
+            Correo electrónico:
+          </label>
+          <input type="email" name="email" value={formData.email} onChange={handleInputChange} />
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
+    </>
+  )
+}
+
+export default UserForm
