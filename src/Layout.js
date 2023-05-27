9fdcdbec6afbe5cf9795581045e58b92960b24d4
jssatorres <juansataz.dev@gmail.com>
2023-05-27 10:11:04 +0200

    feat: add Layout entry component

---
+++ src/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from '../components/header'
+import Footer from '../components/footer'
+
+const Layout = () => (
+  <div>
+    <Header />
+    <Outlet />
+    <Footer />
+  </div>
+)
+
+export default Layout
