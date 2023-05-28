8aa9598a6408b2835193dd300c19dac4f967f7fc
jssatorres <juansataz.dev@gmail.com>
2023-05-28 11:34:03 +0200

    Feat: Footer add component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+<footer className="bg-gray-900 py-4 fixed bottom-0 left-0 w-full">
+      <div className="container mx-auto flex justify-center items-center">
+        <p className="text-white">
+          Desarrollado por Juan Sanchez<a className="text-blue-500 hover:text-blue-600" href="https://github.com/JSSatorres/testing-library-React" target="_self"> Codigo del proyecto</a>
+        </p>
+      </div>
+    </footer>
   )
 }
 
