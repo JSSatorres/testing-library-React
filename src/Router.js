3827bff14e709bbc01bf92ccc1c678c3fccf7616
jssatorres <juansataz.dev@gmail.com>
2023-05-30 18:19:28 +0200

    Feat: add characterCadd route

--- src/Router.js
+++ src/Router.js
@@ -4,6 +4,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './layout'
 import Home from './Pages/home'
 import NoMatch from './Pages/noMatch'
+import CharacterCard from './components/characterCard'
+
 // import AboutMe from './pages/aboutMe'
 // import ContactPage from './pages/contact'
 // import ProjectsPage from './pages/projectsPage'
@@ -16,6 +18,10 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Home />,
+      },
+      {
+        path: 'character/:id', // Ruta dinámica para el detalle del personaje
+        element: <CharacterCard />,
       },
       {
         path: '*',
