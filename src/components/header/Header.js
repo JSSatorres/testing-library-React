8319e7a82a329ab41cde8a4ccd2aeb10762a9162
jssatorres <juansataz.dev@gmail.com>
2023-06-03 09:06:17 +0200

    fix: (Header) add navigation by Link

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,23 +1,18 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  const handleLogin = () =>{
-    console.log('click login');
-  }
+
   const handleLogout = () =>{
     console.log('click logout');
   }
+  
   return (
     <header className="bg-gray-900 py-4">
       <div className="container mx-auto flex justify-between items-center px-4">
-        <h1 className="text-2xl font-bold text-white">Prueba</h1>
+        <h1 className="text-2xl font-bold text-white"> <Link to="/">Prueba</Link></h1>
         <div>
-          <button 
-            onClick={handleLogin}
-            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded mr-2"
-          >
-            Login
-          </button>
+          <Link to="/login" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded mr-2">Login</Link>    
           <button 
             onClick={handleLogout}
             className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded"
