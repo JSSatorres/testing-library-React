f2886da45361a7b1e14f4886ca7c9422543fed8a
jssatorres <juansataz.dev@gmail.com>
2023-09-14 17:44:13 +0200

    feat: upload app

--- src/services/fetchCharacter.js
+++
@@ -1,9 +0,0 @@
-export const fetchData = async (id) => {
-  try {
-    const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    throw new Error('Error al obtener los datos');
-  }
-};
