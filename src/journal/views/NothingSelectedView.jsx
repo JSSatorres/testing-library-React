aee97ab0ddf77e0df9da3f07a8efa14351fb0429
jssatorres <juansataz.dev@gmail.com>
2023-09-22 12:43:08 +0200

    fix: pass standarjs

--- src/journal/views/NothingSelectedView.jsx
+++ src/journal/views/NothingSelectedView.jsx
@@ -4,12 +4,12 @@ import { StarOutline } from '@mui/icons-material'
 export const NothingSelectedView = () => {
   return (
     <Grid
-      className="animate__animated animate__fadeIn animate__faster"
+      className='animate__animated animate__fadeIn animate__faster'
       container
       spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
+      direction='column'
+      alignItems='center'
+      justifyContent='center'
       sx={{
         minHeight: 'calc(100vh - 110px)',
         backgroundColor: 'primary.main',
@@ -20,7 +20,7 @@ export const NothingSelectedView = () => {
         <StarOutline sx={{ fontSize: 100, color: 'white' }} />
       </Grid>
       <Grid item xs={12}>
-        <Typography color="white" variant="h5">
+        <Typography color='white' variant='h5'>
           Selecciona o crea una entrada
         </Typography>
       </Grid>
